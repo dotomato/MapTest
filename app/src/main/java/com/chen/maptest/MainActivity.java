5a4f4d01ba97d42aa18f8a3918a0538071ed4c89
chen <dotomato@163.com>
2017-02-03 21:55:38 +0800

    引入rxandroid

--- app/src/main/java/com/chen/maptest/MainActivity.java
+++ app/src/main/java/com/chen/maptest/MainActivity.java
@@ -1,26 +1,123 @@
 package com.chen.maptest;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.AMapOptions;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.Projection;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.cloud.CloudItemDetail;
+import com.amap.api.services.cloud.CloudResult;
+import com.amap.api.services.cloud.CloudSearch;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationSource, AMapLocationListener, AMap.OnMapTouchListener, CloudSearch.OnCloudSearchListener {
+
+
+    private final static String TAG = "MainActivity";
+
+    private final static int WRITE_COARSE_LOCATION_REQUEST_CODE = 0;
 
     @BindView(R.id.map)
     public MapView mMapView;
 
+    private AMap aMap;
+    private UiSettings mUiSettings;
+
+    private OnLocationChangedListener mListener;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private Projection mProjection;
+    private CloudSearch mCloudSearch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initPremisstion();
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
+        initAmap();
+    }
+
+    private void initPremisstion(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    WRITE_COARSE_LOCATION_REQUEST_CODE);//自定义的code
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    WRITE_COARSE_LOCATION_REQUEST_CODE);//自定义的code
+        }
+    }
+
+    private void initAmap(){
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+        // 设置定位监听
+        aMap.setLocationSource(MainActivity.this);
+// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        aMap.setMyLocationEnabled(true);
+// 设置定位的类型为定位模式，有定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+        //设置地图手势监听器
+        aMap.setOnMapTouchListener(this);
+
+        mUiSettings = aMap.getUiSettings();
+        //设置滑动手势
+        mUiSettings.setScrollGesturesEnabled(true);
+        //设置缩放手势
+        mUiSettings.setZoomGesturesEnabled(true);
+        //设置倾斜手势
+        mUiSettings.setTiltGesturesEnabled(false);
+        //设置旋转手势
+        mUiSettings.setRotateGesturesEnabled(true);
+        //设置放大缩小指示器
+        mUiSettings.setZoomControlsEnabled(true);
+        //设置指南针
+        mUiSettings.setCompassEnabled(true);
+        //设置定位按钮
+        mUiSettings.setMyLocationButtonEnabled(true);
+        //设置比例尺控件
+        mUiSettings.setScaleControlsEnabled(true);
+        //设置logo位置
+        mUiSettings.setLogoPosition(AMapOptions.LOGO_POSITION_BOTTOM_LEFT);
+
+        //得到坐标转换器
+        mProjection = aMap.getProjection();
+
+        mCloudSearch = new CloudSearch(this);// 初始化查询类
+        mCloudSearch.setOnCloudSearchListener(this);// 设置回调函数
+        // 设置中心点及检索范围
+
     }
 
     @Override
@@ -28,6 +125,9 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
+        if(null != mlocationClient){
+            mlocationClient.onDestroy();
+        }
     }
     @Override
     protected void onResume() {
@@ -46,5 +146,82 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mlocationClient == null) {
+            //初始化定位
+            mlocationClient = new AMapLocationClient(this);
+            //初始化定位参数
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位回调监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();//启动定位
+        }
+    }
+
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null && amapLocation != null) {
+            if (amapLocation != null
+                    && amapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+                makeQuery(amapLocation);
+            } else {
+                String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr",errText);
+            }
+        }
+    }
+
+    private void makeQuery(AMapLocation amapLocation){
+        CloudSearch.SearchBound bound = new CloudSearch.SearchBound(new LatLonPoint(
+                amapLocation.getLatitude(), amapLocation.getLongitude()), 4000);
+        //设置查询条件 mTableID是将数据存储到数据管理台后获得。
+        try {
+            CloudSearch.Query mQuery = new CloudSearch.Query("58941b3a7bbf195ae87f2565", "医院", bound);
+            mCloudSearch.searchCloudAsyn(mQuery);
+        } catch (AMapException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTouch(MotionEvent motionEvent) {
+        Log.d(TAG,"onTouch "+motionEvent.toString());
+    }
+
+    @Override
+    public void onCloudSearched(CloudResult cloudResult, int i) {
+        Log.d(TAG,"onCloudSearched "+cloudResult.toString()+" "+cloudResult.getTotalCount());
+    //    Log.d(TAG,"detail "+cloudResult.getClouds().get(0).);
+
+
+    }
+
+    @Override
+    public void onCloudItemDetailSearched(CloudItemDetail cloudItemDetail, int i) {
+        Log.d(TAG,"onCloudItemDetailSearched "+cloudItemDetail.toString()+" "+i);
+
     }
 }
