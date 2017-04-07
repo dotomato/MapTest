e6b5eff6f304378b7a6ec8960eaec657a02381d8
chen <dotomato@163.com>
2017-04-07 15:53:03 +0800

    干爆它娘的RecycleView!老子不玩了!老子用回ListView,RecyvleView爱滚哪滚哪!!!!MLGB的

--- app/src/main/java/com/chen/maptest/ScanMessageRv.java
+++ app/src/main/java/com/chen/maptest/ScanMessageRv.java
@@ -1,7 +1,10 @@
 package com.chen.maptest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,15 +14,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.chen.maptest.Manager.MyUM;
 import com.chen.maptest.MyModel.PointSimpleData;
 import com.chen.maptest.MyModel.Userinfo;
 import com.chen.maptest.MyModel.UserinfoResult;
 import com.chen.maptest.MyServer.MyAction1;
 import com.chen.maptest.MyServer.Myserver;
+import com.chen.maptest.MyView.ListViewItemData;
+import com.chen.maptest.MyView.MyListViewAdapter;
 import com.chen.maptest.MyView.OutlineProvider;
 import com.chen.maptest.R;
 import com.chen.maptest.Utils.MyUtils;
@@ -29,6 +39,8 @@ import com.zhy.adapter.recyclerview.base.ViewHolder;
 import com.zhy.adapter.recyclerview.wrapper.EmptyWrapper;
 import com.zhy.adapter.recyclerview.wrapper.HeaderAndFooterWrapper;
 import com.zhy.adapter.recyclerview.wrapper.LoadMoreWrapper;
+
+import net.steamcrafted.materialiconlib.MaterialIconView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,10 +58,10 @@ import rx.schedulers.Schedulers;
  * Copyright *
  */
 
-public class ScanMessageRv extends RecyclerView {
+public class ScanMessageRv extends ListView {
     private Context mContext;
 
-    private CommonAdapter<PointSimpleData> mAdapter;
+    private MyListViewAdapter2 mAdapter;
 
     private List<PointSimpleData> mDatas;
     private final String TAG = "ScanMessageRv";
@@ -71,72 +83,72 @@ public class ScanMessageRv extends RecyclerView {
     }
 
     public void initview(){
-
-
-        setLayoutManager(new LinearLayoutManager(mContext , LinearLayoutManager.VERTICAL, false));
-        setHasFixedSize(false);
-
-        mAdapter = new CommonAdapter<PointSimpleData>(mContext, R.layout.layout_scan_item, mDatas){
-            @Override
-            protected void convert(final com.zhy.adapter.recyclerview.base.ViewHolder holder,final PointSimpleData psd, int position) {
-                UserIconWarp.just(mContext, psd.userIcon, (ImageView) holder.getView(R.id.usericon));
-                holder.setText(R.id.username, psd.userName);
-
-                OutlineProvider.setOutline(holder.getConvertView(),OutlineProvider.SHAPE_RECT);
-                OutlineProvider.setOutline(holder.getView(R.id.usericon),OutlineProvider.SHAPE_OVAL);
-
-                holder.setText(R.id.smallmsg,psd.smallMsg);
-
-                holder.setOnClickListener(R.id.item_root, new OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (mOnRecyclerViewItemClickListener != null) {
-                            mOnRecyclerViewItemClickListener.onItemClickListener(view, psd);
-                        }
-                    }
-                });
-            }
-        };
-
+        mAdapter = new MyListViewAdapter2(mContext,R.layout.layout_scan_item,mDatas);
         setAdapter(mAdapter);
+        setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    if (mOnRecyclerViewItemClickListener != null) {
+                        PointSimpleData psd = mDatas.get(i);
+                        mOnRecyclerViewItemClickListener.onItemClickListener(view, psd);
+                    }
+            }
+        });
+        setAdapter(mAdapter);
+    }
 
-//        DefaultItemAnimator da = new DefaultItemAnimator();
-//        da.setRemoveDuration(0);
-//        da.setAddDuration(350);
-//        setItemAnimator(da);
+    class MyListViewAdapter2 extends ArrayAdapter<PointSimpleData> {
+
+
+        private int resourceID;
+        private LayoutInflater mLayoutInflater;
+        private Context mContext;
+        private List<PointSimpleData> inerData;
+
+        public MyListViewAdapter2(@NonNull Context context, @LayoutRes int resource, @NonNull List<PointSimpleData> objects) {
+            super(context, resource, objects);
+            mContext = context;
+            resourceID = resource;
+            inerData = objects;
+            mLayoutInflater = LayoutInflater.from(mContext);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            MyListViewItem2Holder holder;
+            PointSimpleData psd = inerData.get(position);
+            if (convertView==null){
+                convertView = mLayoutInflater.inflate(resourceID,ScanMessageRv.this,false);
+                holder = new MyListViewItem2Holder();
+                holder.userIcon = (ImageView)convertView.findViewById(R.id.usericon);
+                holder.userName = (TextView)convertView.findViewById(R.id.username);
+                holder.smallMsg = (TextView)convertView.findViewById(R.id.smallmsg);
+                OutlineProvider.setOutline(convertView,OutlineProvider.SHAPE_RECT);
+                OutlineProvider.setOutline(holder.userIcon,OutlineProvider.SHAPE_OVAL);
+                convertView.setTag(holder);
+            } else{
+                holder = (MyListViewItem2Holder)convertView.getTag();
+            }
+
+            UserIconWarp.just(mContext, psd.userIcon, holder.userIcon);
+            holder.userName.setText(psd.userName);
+            holder.smallMsg.setText(psd.smallMsg);
+            return convertView;
+        }
+
+        private class MyListViewItem2Holder {
+            ImageView userIcon;
+            TextView userName;
+            TextView smallMsg;
+        }
+
     }
 
     public void setScanData(final List<PointSimpleData> points){
-        int j = mDatas.size();
         mDatas.clear();
+        mDatas.addAll(points);
         mAdapter.notifyDataSetChanged();
-
-        for (int i=0;i<points.size();i++){
-            mDatas.add(i,points.get(i));
-            mAdapter.notifyItemInserted(i);
-        }
-//        mDatas.addAll(points);
-//        mAdapter.notifyItemRangeInserted(0,mDatas.size());
-
-//        Observable.interval(200,200, TimeUnit.MILLISECONDS)
-//                .take(points.size())
-//                .map(new Func1<Long, PointSimpleData>() {
-//                    @Override
-//                    public PointSimpleData call(Long aLong) {
-//                        return points.get(aLong.intValue());
-//                    }
-//                })
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Action1<PointSimpleData>() {
-//                    @Override
-//                    public void call(PointSimpleData psd) {
-//                        Log.d(TAG,psd.smallMsg);
-//                        mDatas.add(psd);
-//                        mAdapter.notifyItemInserted(mDatas.size()-1);
-//                    }
-//        });
     }
-
 
     public interface OnRecyclerViewItemClickListener {
         void onItemClickListener(View View, PointSimpleData psd);
